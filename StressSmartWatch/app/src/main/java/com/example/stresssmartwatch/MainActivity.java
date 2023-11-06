d8cb38620780e940209d194efc7af8c6b45ad594
Alexia Rangel K <73617301+alexiarangelk@users.noreply.github.com>
2023-11-05 23:13:48 -0500

    Some Starting Sensing Stuff
    Trying things out

--- StressSmartWatch/app/src/main/java/com/example/stresssmartwatch/MainActivity.java
+++ StressSmartWatch/app/src/main/java/com/example/stresssmartwatch/MainActivity.java
@@ -4,12 +4,20 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.util.Log;
+
 import com.example.stresssmartwatch.databinding.ActivityMainBinding;
 
 public class MainActivity extends Activity {
 
     private TextView mTextView;
     private ActivityMainBinding binding;
+    private SensorManager mSensorManager;
+    private Sensor mHeartSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +27,37 @@ public class MainActivity extends Activity {
         setContentView(binding.getRoot());
 
         mTextView = binding.text;
+
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mHeartSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(mSensorEventListener, mHeartSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(mSensorEventListener);
+    }
+
+    private SensorEventListener mSensorEventListener = new SensorEventListener() {
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            final SensorEvent event1=event;
+            mTextView.setText(Float.toString(event1.values[0]));
+        }
+
+
+    };
 }
+
+
+
