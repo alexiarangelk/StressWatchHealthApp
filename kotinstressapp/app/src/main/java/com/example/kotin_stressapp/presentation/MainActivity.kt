bcd63f832107daccf9c840d83689adb27d217b0e
Alexia Rangel K <73617301+alexiarangelk@users.noreply.github.com>
2023-11-13 22:06:57 -0500

    Some Cleanup

--- kotinstressapp/app/src/main/java/com/example/kotin_stressapp/presentation/MainActivity.kt
+++ kotinstressapp/app/src/main/java/com/example/kotin_stressapp/presentation/MainActivity.kt
@@ -1,23 +1,19 @@
 package com.example.kotin_stressapp.presentation
 
-import android.graphics.Color
 import android.util.Log
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.wear.compose.material.Button
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
-import com.example.kotin_stressapp.R
 import com.example.kotin_stressapp.presentation.theme.KotinstressappTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -30,14 +26,6 @@ import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
 
-/*
-Stopped = 0
-Running = 1
-Paused = 2
-*/
-
-//    var appState: AppState = AppState.STOPPED
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -45,86 +33,6 @@ Paused = 2
         }
     }
 
-//    @Composable
-//    fun WearApp(
-//        greetingName: String
-//    ) {
-//        KotinstressappTheme {
-//            /* If you have enough items in your list, use [ScalingLazyColumn] which is an optimized
-//             * version of LazyColumn for wear devices with some added features. For more information,
-//             * see d.android.com/wear/compose.
-//             */
-//            var appState = remember { mutableStateOf(AppState.STOPPED) }
-//            //var appState: AppState by rememberMutableStateOf(AppState.STOPPED)
-//
-//            Column(
-//                modifier = Modifier
-//                    .fillMaxWidth()
-//                    .fillMaxHeight()
-//                    .fillMaxSize()
-//                    .padding(8.dp)
-//                    .background(MaterialTheme.colors.background),
-//
-//                verticalArrangement = Arrangement.Center,
-//                horizontalAlignment = Alignment.CenterHorizontally
-//            ) {
-//                Greeting(greetingName = greetingName)
-//
-//                Row(
-//                    modifier = Modifier
-//                        .padding(8.dp),
-//                    horizontalArrangement = Arrangement.spacedBy(8.dp)
-//                ) {
-//                    // Row content
-//                    if (appState.value == AppState.STOPPED) { //while app is not running
-//                        Button(
-//                            onClick = { //running the app
-//                                Log.d("Button - Start", "button clicked")
-//                                appState.value = AppState.RUNNING
-//                                appRunning(appState)
-//                            }
-//                        ) {
-//                            Text("Start")
-//                        }
-//                    } else { //while app is running OR paused
-//                        Log.d("Button - Stop/Pause", "button should have changed")
-//                        Button(
-//                            onClick = { //stopping the app
-//                                appState.value = AppState.STOPPED
-//                                appStopping(appState)
-//
-//                            }
-//                        ) {
-//                            Text("Stop")
-//                        }
-//
-//                        //Resume + Pause buttons
-//                        Button(
-//                            onClick = {
-//                                if (appState.value == AppState.PAUSE) { //app is paused
-//                                    //unpause it
-//                                    appState.value = AppState.RUNNING
-//                                } else { //app is running
-//                                    //pause it
-//                                    appState.value = AppState.PAUSE
-//                                    appPausing(appState)
-//                                }
-//                            }
-//                        ) {
-//                            if (appState.value == AppState.PAUSE) { //app is paused
-//                                //prepare for resume
-//                                Text("Resume")
-//                            } else { //app is running
-//                                //prepare for pause
-//                                Text("Pause")
-//                            }
-//
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//    }
 
     @Composable
     fun WearApp(
